2297ea87438254bf26dbec2db9a4db05aa4dd345
Leonardo <125615006+leoplibero@users.noreply.github.com>
2023-11-21 20:28:33 -0300

    Update 21/11
    Janelas pop-up redimensionadas.

--- apps/app.js
+++ apps/app.js
@@ -1,4 +1,17 @@
-function Abrir()
+function Abrir1()
 		{
-			window.open('popup/PopUpGrecia.html', 'Janelinha', 'height=570,width=320,toolbar=no,scrollbars=no,menubar=no,status=no');
+			window.open('popup/PopUpItalia.html', 'Janelinha', 'height=570,width=320, toolbar=yes, scrollbars=yes, menubar=yes, status=no, location=yes, left=600, screenX=600, top=150, screenY=150');
+		}
+
+function Abrir2()
+		{
+			window.open('popup/PopUpBahia.html', 'Janelinha', 'height=570,width=320, toolbar=yes, scrollbars=yes, menubar=yes, status=no, location=yes, left=600, screenX=600, top=150, screenY=150');
+		}
+function Abrir3()
+		{
+			window.open('popup/PopUpGrecia.html', 'Janelinha', 'scrollbars=no, height=570,width=320, toolbar=yes, scrollbars=no, menubar=no, status=no, location=yes, left=600, screenX=600, top=150, screenY=150');
+		}
+function Abrir4()
+		{
+			window.open('popup/PopUpCaribe.html', 'Janelinha', 'height=570,width=320, toolbar=yes, scrollbars=yes, menubar=yes, status=no, location=yes, left=600, screenX=600, top=150, screenY=150');
 		}
